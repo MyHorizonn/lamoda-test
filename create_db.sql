e4ea79f3d6809f59f4d0418bd9c42af4bd804d70
Nikita Nosikovskiy <nik.nosikovskiy@gmail.com>
2024-03-29 01:42:01 +0300

    remove comments

--- create_db.sql
+++ create_db.sql
@@ -1,12 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 16.2
--- Dumped by pg_dump version 16.2
-
--- Started on 2024-03-29 01:16:51 MSK
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -22,10 +13,6 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
---
--- TOC entry 215 (class 1259 OID 16399)
--- Name: goods; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.goods (
     uuid uuid NOT NULL,
@@ -37,10 +24,6 @@ CREATE TABLE public.goods (
 
 ALTER TABLE public.goods OWNER TO postgres;
 
---
--- TOC entry 217 (class 1259 OID 16405)
--- Name: store; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.store (
     id integer NOT NULL,
@@ -51,10 +34,6 @@ CREATE TABLE public.store (
 
 ALTER TABLE public.store OWNER TO postgres;
 
---
--- TOC entry 216 (class 1259 OID 16404)
--- Name: storage_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.storage_id_seq
     AS integer
@@ -67,44 +46,16 @@ CREATE SEQUENCE public.storage_id_seq
 
 ALTER SEQUENCE public.storage_id_seq OWNER TO postgres;
 
---
--- TOC entry 3600 (class 0 OID 0)
--- Dependencies: 216
--- Name: storage_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
 
 ALTER SEQUENCE public.storage_id_seq OWNED BY public.store.id;
 
 
---
--- TOC entry 3447 (class 2604 OID 16408)
--- Name: store id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.store ALTER COLUMN id SET DEFAULT nextval('public.storage_id_seq'::regclass);
 
-
---
--- TOC entry 3449 (class 2606 OID 16403)
--- Name: goods goods_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.goods
     ADD CONSTRAINT goods_pkey PRIMARY KEY (uuid);
 
 
---
--- TOC entry 3451 (class 2606 OID 16410)
--- Name: store storage_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.store
     ADD CONSTRAINT storage_pkey PRIMARY KEY (id);
-
-
--- Completed on 2024-03-29 01:16:51 MSK
-
---
--- PostgreSQL database dump complete
---
-
