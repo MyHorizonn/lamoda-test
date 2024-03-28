93f14041a15a023942404f2f786927f22dba197d
Nikita Nosikovskiy <nik.nosikovskiy@gmail.com>
2024-03-29 02:00:56 +0300

    docker fix

--- cmd/goods/main.go
+++ cmd/goods/main.go
@@ -18,7 +18,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), "host.docker.internal", os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	dbOp, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalln(err)
